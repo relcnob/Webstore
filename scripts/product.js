58be0146301bed3b27ff5b4c31655010986de1a8
Fryderyk Boncler <f.relcnob@gmail.com>
2022-02-04 12:12:37 +0100

    added responsive data to product page

---
+++ scripts/product.js
@@ -0,0 +1,49 @@
+const url = "https://kea-alt-del.dk/t7/api/products/1163";
+//fetch the data
+fetch(url)
+  .then((res) => res.json())
+  .then((data) => showProduct(data));
+//populate the page
+
+function showProduct(product) {
+  document.querySelector(".breadcrumbs .brand a").textContent =
+    product.brandname;
+  document.querySelector(".breadcrumbs .productName").textContent =
+    product.productdisplayname;
+  document.querySelector(
+    "img.productImage"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/1000/${product.id}.webp`;
+  document.querySelector("img.productImage").alt = product.productdisplayname;
+
+  document.querySelector(".purchaseBox h1").textContent =
+    product.productdisplayname;
+
+  document.querySelector(".purchaseBox h2").textContent =
+    product.brandname + " | " + product.articletype;
+
+  document.querySelector(".purchaseBox h3").textContent =
+    "DKK " + product.price + ",-";
+
+  if (product.styledesc == null) {
+    document.querySelector(".purchaseBox p").textContent = "-";
+  } else {
+    document.querySelector(".purchaseBox p").textContent = product.styledesc;
+  }
+
+  document.querySelector(".productDesc div dl dd:nth-of-type(1)").textContent =
+    product.productdisplayname;
+  document.querySelector(".productDesc div dl dd:nth-of-type(2)").textContent =
+    product.id;
+  document.querySelector(".productDesc div dl dd:nth-of-type(3)").textContent =
+    product.basecolour;
+  if (product.materialcaredesc === null) {
+    document.querySelector(
+      ".productDesc div dl dd:nth-of-type(4)"
+    ).textContent = "-";
+  } else {
+    document.querySelector(".productDesc div dl dd:nth-of-type(4)").innerHTML =
+      product.materialcaredesc;
+  }
+  document.querySelector(".brandInfo h1").textContent = product.brandname;
+  document.querySelector(".brandInfo p").textContent = product.brandbio;
+}
