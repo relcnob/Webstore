0cf0debbb5be24ccda1129a63f4d31efca5eff9d
Fryderyk Boncler <f.relcnob@gmail.com>
2022-02-07 12:04:37 +0100

    added responsivity to productlist

---
+++ scripts/productlist.js
@@ -0,0 +1,67 @@
+const url = "https://kea-alt-del.dk/t7/api/products";
+
+fetch(url)
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    handleProductList(data);
+  });
+
+function handleProductList(data) {
+  data.forEach(showProduct);
+}
+
+/*
+        <article class="productCard">
+          <p class="saleLabel">SALE</p>
+          <img
+            src="https://kea-alt-del.dk/t7/images/webp/640/1528.webp"
+            alt="t-shirt"
+          />
+          <h3>Black Fleece Jacket</h3>
+          <p class="subtitle">Jackets | Puma</p>
+          <div class="discount">
+            <p>DKK 2000,-</p>
+            <p>-49%</p>
+          </div>
+          <p class="price">DKK 3999,-</p>
+          <a href="product.html">BUY NOW</a>
+        </article>
+*/
+
+function showProduct(item) {
+  //grab template
+  const template = document.querySelector("#productTemplate").content;
+  //clone it
+  const copy = template.cloneNode(true);
+  //change content
+
+  copy.querySelector(
+    ".subtitle"
+  ).textContent = `${item.articletype} | ${item.brandname}`;
+  copy.querySelector(
+    ".productCard img"
+  ).src = `https://kea-alt-del.dk/t7/images/webp/640/${item.id}.webp`;
+  copy.querySelector(".productCard img").alt = `${item.productdisplayname}`;
+  copy.querySelector(
+    ".productCard h3"
+  ).textContent = `${item.productdisplayname}`;
+  if (item.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (item.discount) {
+    copy.querySelector("article").classList.add("onSale");
+    copy.querySelector(".discount p").textContent =
+      "DKK " + (item.discount / 100) * item.price;
+    copy.querySelector(
+      ".discount p:nth-of-type(2)"
+    ).textContent = `- ${item.discount} %`;
+  }
+  copy.querySelector(".price").textContent = item.price + ",-";
+
+  //grab parent
+  const parent = document.querySelector("main");
+  //append
+  parent.appendChild(copy);
+}
